84766a494b13ba0715504496b8c5e542465f70c5
Cody Bonney <me@codybonney.com>
2013-06-24 17:05:52 -0600

    Added comment

--- shell/pngquant/optimize-and-replace.sh
+++ shell/pngquant/optimize-and-replace.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# http://pngquant.org/
+# http://pngquant.org
 # finds all files with a png extension in the current directory and replaces them with optimized versions
 pngquant --quality=90-100 --speed=1 ./*.png --ext .png -f
