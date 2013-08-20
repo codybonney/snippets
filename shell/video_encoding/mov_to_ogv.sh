473a335aa9adca8c019916d08a3fd27af2493f65
Cody Bonney <me@codybonney.com>
2013-08-20 10:18:36 -0600

    added instructions for installing brew and ffmpeg

--- shell/video_encoding/mov_to_ogv.sh
+++ shell/video_encoding/mov_to_ogv.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# install brew if you haven't already
+# http://brew.sh/
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+# install ffmpeg using brew
+brew install ffmpeg --with-freetype --with-theora --with-libvorbis --with-libvpx --with-rtpdump --with-opencore-amr --with-libvo-aacenc --with-libass --with-openjpeg --with-schroedinger --with-ffplay --with-tools --with-fdk-aac --with-openssl --with-opus
+
+# example video conversion
+# audio bitrate: 128k
+# video bitrate: 1200k
+# width: 730px
+# height: 525px
+
 ffmpeg -i pants.mov \
-  -acodec libvorbis -ac 2 -ab 128K -ar 44100 \
+  -acodec libvorbis -ac 2 -ab 128k -ar 44100 \
   -b:v 1200k -s 730x525 pants.ogv
