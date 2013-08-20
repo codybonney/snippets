14a79c32735df9b7d7e2d72a53a9c994903bea69
Cody Bonney <me@codybonney.com>
2013-08-20 16:54:22 -0600

    Added some callbacks

--- javascript/orientation.js
+++ javascript/orientation.js
@@ -1,6 +1,10 @@
 var Orientation = (function() {
+    var self = this;
 
-    function Orientation() {}
+    function Orientation(args) {
+        self.args = args;
+        Orientation.prototype.callbacks();
+    }
 
     // return true if in portrait
     Orientation.prototype.portrait = function () {
@@ -12,12 +16,43 @@ var Orientation = (function() {
         return window.orientation === 90 || window.orientation === -90;
     };
 
+    Orientation.prototype.callbacks = function () {
+        // onOrientationChange callback
+        if(self.args && self.args.onOrientationChange) {
+            self.args.onOrientationChange();
+        }
+
+        // onPortrait callback
+        if(self.args && self.args.onPortrait && Orientation.prototype.portrait()) {
+            self.args.onPortrait();
+        }
+
+        // onLandscape callback
+        if(self.args && self.args.onLandscape && Orientation.prototype.landscape()) {
+            self.args.onLandscape();
+        }
+    };
+
+    // bind orientation change to callbacks
+    window.onorientationchange = Orientation.prototype.callbacks;
+
     return Orientation;
 })();
 
+
 /*
     // Usage
-    var o = new Orientation();
+    var o = new Orientation({
+        onOrientationChange: function() {
+            console.log('Orientation change');
+        },
+        onPortrait: function() {
+            console.log('portrait mode!');
+        },
+        onLandscape: function() {
+            console.log('landscape mode!');
+        }
+    });
 
     if(o.portrait()) {
         console.log('In portrait mode');
