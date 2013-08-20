f298961b33f8d398eb3fbc774e8f873f7ddeada5
Cody Bonney <me@codybonney.com>
2013-08-20 10:47:02 -0600

    added line breaks

--- shell/video_encoding/mov_to_ogv.sh
+++ shell/video_encoding/mov_to_ogv.sh
@@ -5,7 +5,22 @@
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
 # install ffmpeg using brew
-brew install ffmpeg --with-freetype --with-theora --with-libvorbis --with-libvpx --with-rtpdump --with-opencore-amr --with-libvo-aacenc --with-libass --with-openjpeg --with-schroedinger --with-ffplay --with-tools --with-fdk-aac --with-openssl --with-opus
+brew install ffmpeg \
+    --with-freetype \
+    --with-theora \
+    --with-libvorbis \
+    --with-libvpx \
+    --with-rtpdump \
+    --with-opencore-amr \
+    --with-libvo-aacenc \
+    --with-libass \
+    --with-openjpeg \
+    --with-schroedinger \
+    --with-ffplay \
+    --with-tools \
+    --with-fdk-aac \
+    --with-openssl \
+    --with-opus
 
 # example video conversion
 # audio bitrate: 128k
