a623d31275fe0fac59b67e7ddb11ef59637da7af
Cody Bonney <me@codybonney.com>
2013-08-20 10:48:12 -0600

    added proper spacing

--- shell/video_encoding/mov_to_ogv.sh
+++ shell/video_encoding/mov_to_ogv.sh
@@ -29,5 +29,5 @@ brew install ffmpeg \
 # height: 525px
 
 ffmpeg -i inputFile.mov \
-  -acodec libvorbis -ac 2 -ab 128k -ar 44100 \
-  -b:v 1200k -s 730x525 outputFile.ogv
+    -acodec libvorbis -ac 2 -ab 128k -ar 44100 \
+    -b:v 1200k -s 730x525 outputFile.ogv
