8f32d709635d6c909307c76cfdf7b7a351b82e8c
Cody Bonney <me@codybonney.com>
2013-08-30 16:49:48 -0600

    reduced to single line

--- cli/ffmpeg/convert_from_mov_to_ogv.sh
+++ cli/ffmpeg/convert_from_mov_to_ogv.sh
@@ -28,12 +28,4 @@ brew install ffmpeg \
 # width: 730px
 # height: 525px
 
-ffmpeg -i \
-    inputFile.mov \
-    -acodec libvorbis \
-    -ac 2 \
-    -ab 128k \
-    -ar 44100 \
-    -b:v 1200k \
-    -s 730x525 \
-    outputFile.ogv
+ffmpeg -i inputFile.mov -acodec libvorbis -ac 2 -ab 128k -ar 44100 -b:v 1200k -s 730x525 outputFile.ogv
