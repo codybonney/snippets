72ac261abf845a6fc309094c73fee5429d9437e4
Cody Bonney <me@codybonney.com>
2013-09-13 23:07:14 -0600

    basic php class

---
+++ php/basic_class.php
@@ -0,0 +1,10 @@
+<?php
+
+class basicClass {
+    var $input;
+    var $output;
+
+    function __construct($input) {
+        $this->input = $input;  // This is a valid thing to do in a constructor
+    }
+}
