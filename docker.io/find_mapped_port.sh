06fe08759158785c8ba32c69b87d6e93ead03ac6
Cody Bonney <me@codybonney.com>
2013-10-09 10:09:17 -0600

    finding mapped port

--- docker.io/find_mapped_port.sh
+++ docker.io/find_mapped_port.sh
@@ -6,4 +6,8 @@ docker ps
 # > ID            IMAGE                          COMMAND              ...   PORTS
 # > ecce33b30ebf  gasi/centos-node-hello:latest  node /src/index.js         49160->8080
 
-#In the example above, docker mapped the 8080 port of the container to 49160.
+# In the example above, docker mapped the 8080 port of the container to 49160.
+
+# Now you can call your app using curl (install if needed via: sudo apt-get install curl):
+
+curl -i localhost:49160
