5cdc44ff508b500c2a5d37ef5d1d5a2e368f2ebe
Cody Bonney <me@codybonney.com>
2013-11-18 10:47:15 -0700

    Removed unnecessary variables

--- javascript/get_query_string_parameters.js
+++ javascript/get_query_string_parameters.js
@@ -1,7 +1,8 @@
 var queryStringParameters = (function() {
-	var url = location;
-	var querystring = location.search.slice(1);
-	return querystring.split("&").map(function(qs) {
-		return { "Key": qs.split("=")[0], "Value": qs.split("=")[1], "Pretty Value": decodeURIComponent(qs.split("=")[1]).replace(/\+/g," ") }
+	return location.search.slice(1).split("&").map(function(qs) {
+		return {
+			"Key": qs.split("=")[0],
+			"Value": qs.split("=")[1]
+		}
 	});
 })();
